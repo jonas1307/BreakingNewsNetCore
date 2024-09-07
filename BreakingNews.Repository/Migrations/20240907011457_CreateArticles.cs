573bc3da74379687a57578cf2c64dc49910df433
Jonas Lima de Amorim <dev.amorim@outlook.com>
2024-09-06 22:24:10 -0300

    Adding Migrations

---
+++ BreakingNews.Repository/Migrations/20240907011457_CreateArticles.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BreakingNews.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateArticles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Summary = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Author = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Category = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Body = table.Column<string>(type: "text", nullable: false),
+                    Slug = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    LastUpdateDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UNIQ_Articles_Slug",
+                table: "Articles",
+                column: "Slug",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articles");
+        }
+    }
+}
